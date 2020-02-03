c8411012a70dc6c86bb74e377773c286d5e08056
flightcoded <37447279+flightcoded@users.noreply.github.com>
2020-02-03 23:01:15 +0300

    initial creation

---
+++ examples/parallelogram.cpp
@@ -0,0 +1,36 @@
+
+#include "fly.h"
+
+int main(){
+
+  fly::createWindow("fly demo", 800, 600);
+  fly::initialize(fly::projectionMethod::ORTHO);
+
+  float vertices[] = {
+    //x,   y, z, s, t
+   -100, -50, 0, 0, 1,
+      0,  50, 0, 0, 0,
+    100,  50, 0, 1, 0,
+      0, -50, 0, 1, 1
+  };
+
+  unsigned int indices[] = {
+
+    0, 1, 3,
+    3, 1, 2
+  };
+
+  fly::Thing parallelogram;
+  parallelogram.initialize(vertices, sizeof(vertices), indices, sizeof(indices));
+
+  fly::fill(1, 0.5, 0.5);
+
+  for(; fly::running(); fly::update()){
+
+    fly::translate(fly::width/2, fly::height/2);
+    parallelogram.display();
+  }
+
+  fly::terminate();
+  return 0;
+}
