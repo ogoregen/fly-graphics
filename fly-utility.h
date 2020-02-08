fddb76333d4a9e7b12e5afb4885c98cc36911ef8
flightcoded <37447279+flightcoded@users.noreply.github.com>
2020-02-08 22:03:41 +0300

    added namespace

--- fly-utility.h
+++ fly-utility.h
@@ -1,10 +1,14 @@
 
 #pragma once
 
+namespace fly{
+
 double map(double x, double r1, double r2, double a1, double a2){
 
   double p = (r2 - r1) / (x - r1);
   double y = ((a2 - a1) / p) + a1;
   return y;
+}
+
 }
 
